82d9ef6d54146993f5dd380dd27439431d269186
Leonard Morrison <lmohamed1@studentmail.nait.ca>
2022-09-03 23:24:38 -0600

    Initialization of Backend

---
+++ backend/ATGMovieAwards/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AtgmovieawardsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ATGMovieAwards'
---
+++ backend/ATGMovieAwards/models.py
@@ -0,0 +1,29 @@
+# import jsonfield
+from django.db import models
+
+
+# Create your models here.
+
+class Database(models.Model):
+    Movie_Title = models.CharField(max_length=120)
+    Year = models.models.models.IntegerField()
+    Genre = models.CharField(max_length=100)
+    DateReleased = models.CharField(max_length=20)
+    imdbRating = models.CharField()
+    Runtime = models.CharField(max_length=10)
+    Director = models.CharField(max_length=50)
+    Writer = models.CharField(max_length=200)
+    Actors = models.CharField(max_length=1000)
+    Plot = models.TextField()
+    Language = models.CharField( max_length=50)
+    Country = models.CharField(max_length=100)
+    Awards = models.CharField(max_length=50)
+    Ratings = models.CharField()
+    MetaScore = models.CharField()
+    imdbVotes = models.CharField()
+    DVD = models.CharField()
+    BoxOffice = models.CharField()
+    Production = models.CharField()
+    Website = models.CharField()
+    Response = models.CharField()
+#    JSONResponse = jsonfield.JSONField()
