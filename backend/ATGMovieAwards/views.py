1cedf504488823143b3fb5a0365290e1273ed59d
Leonard Morrison <lmohamed1@studentmail.nait.ca>
2022-09-05 02:11:56 -0600

    Adding URLs and Views

--- backend/ATGMovieAwards/views.py
+++ backend/ATGMovieAwards/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import logout, authenticate, login
+from django.conf import settings
+from django.shortcuts import redirect
 
 
 # Create your views here.
 def index(response):
     return HttpResponse("Welcome to the ATG Movie Awards!")
+
+def login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        # Redirect to a success page.
+        
+    else:
+        # Return an 'invalid login' error message.
+        
+
+def logout(request):
+    logout(request)
+
+
+
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+from ATGMovieAwards import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', views.index)
+    path('index/', views.index),
+    path('logout/', views.logout)
+    path('login/', views.login)
 ]
